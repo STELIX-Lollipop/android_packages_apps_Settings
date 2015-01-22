e646aa262df4601e403da8a00c34bdf7355c8f26
Roman Birg <roman@cyngn.com>
2015-01-22 10:55:38 -0800

    Settings: allow Profiles to be launched externally
    Change-Id: I620be76139989722ac722f00259425c237a1c66e
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -89,6 +89,7 @@ import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.notification.NotificationAppList;
 import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.profiles.NFCProfileTagCallback;
+import com.android.settings.profiles.ProfilesSettings;
 import com.android.settings.quicklaunch.QuickLaunchSettings;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
@@ -313,8 +314,9 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),
-            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
-            CarbonCustomization.class.getName()
+            CarbonCustomization.class.getName(),
+            ProfilesSettings.class.getName(),
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName()
     };
 
 
