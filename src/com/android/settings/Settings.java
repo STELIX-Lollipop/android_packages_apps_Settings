69de6e0e665b0c8609b1a16760d001d83878e0a0
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-22 13:26:47 +0000

    Settings - QS: add newly added tiles
    * Battery saver
    * Brightness
    * Expanded desktop
    
    * also add notifications tile
    
    Change-Id: I64956333bd22d26db6ce72a6149b496d4542bfec
    
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -103,7 +103,9 @@ public class Settings extends SettingsActivity {
     public static class ApnSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
     public static class ProfilesSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
+
 }
 
