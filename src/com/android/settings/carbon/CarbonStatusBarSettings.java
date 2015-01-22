0fcfd9dfcf38d87fd906b8b1557bc25a37f6d97d
STELIX <ssspinni@gmail.com>
2015-01-22 16:56:36 +0100

    Revert "Status Bar Clock: rewrite for lollipop and add left clock (2/2)"
    This reverts commit 81b7e32d029a937c27ae25baffe7a2a72c8ba518.

--- src/com/android/settings/carbon/CarbonStatusBarSettings.java
+++ src/com/android/settings/carbon/CarbonStatusBarSettings.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2014/2015 The CyanogenMod/CarbonROM Project
+* Copyright (C) 2014 The CyanogenMod/CarbonROM Project
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ package com.android.settings.carbon;
 
 import android.app.ActivityManager;
 import android.content.ContentResolver;
-import android.content.res.Configuration;
 import android.database.ContentObserver;
 import android.os.Bundle;
 import android.os.Handler;
@@ -29,8 +28,6 @@ import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
-import android.text.format.DateFormat;
-import android.view.View;
 import android.util.Log;
 
 import com.android.settings.R;
@@ -44,8 +41,6 @@ public class CarbonStatusBarSettings extends SettingsPreferenceFragment
 
     private static final String TAG = "CarbonStatusBarSettings";
 
-    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
-    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
@@ -56,8 +51,6 @@ public class CarbonStatusBarSettings extends SettingsPreferenceFragment
     private static final String KEY_TRAFFIC_MONITOR = "traffic_monitor";
     private static final String KEY_BATTERY_BAR = "status_bar_battery_bar";
 
-    private ListPreference mStatusBarClock;
-    private ListPreference mStatusBarAmPm;
     private PreferenceScreen mClockStyle;
     private PreferenceScreen mTrafficMonitor;
     private PreferenceScreen mBatteryBar;
@@ -72,8 +65,6 @@ public class CarbonStatusBarSettings extends SettingsPreferenceFragment
 
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mStatusBarClock = (ListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
-        mStatusBarAmPm = (ListPreference) findPreference(STATUS_BAR_AM_PM);
         mClockStyle = (PreferenceScreen) getPreferenceScreen().findPreference(KEY_STATUS_BAR_CLOCK);
         mTrafficMonitor = (PreferenceScreen) getPreferenceScreen().findPreference(KEY_TRAFFIC_MONITOR);
         mBatteryBar = (PreferenceScreen) getPreferenceScreen().findPreference(KEY_BATTERY_BAR);
@@ -81,23 +72,6 @@ public class CarbonStatusBarSettings extends SettingsPreferenceFragment
         mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
         mStatusBarBatteryShowPercent =
                 (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
-
-        int clockStyle = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_CLOCK, 1);
-        mStatusBarClock.setValue(String.valueOf(clockStyle));
-        mStatusBarClock.setSummary(mStatusBarClock.getEntry());
-        mStatusBarClock.setOnPreferenceChangeListener(this);
-
-        if (DateFormat.is24HourFormat(getActivity())) {
-            mStatusBarAmPm.setEnabled(false);
-            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
-        } else {
-            int statusBarAmPm = Settings.System.getInt(resolver,
-                    Settings.System.STATUS_BAR_AM_PM, 2);
-            mStatusBarAmPm.setValue(String.valueOf(statusBarAmPm));
-            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntry());
-            mStatusBarAmPm.setOnPreferenceChangeListener(this);
-        }
 
         int batteryStyle = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
@@ -136,20 +110,6 @@ public class CarbonStatusBarSettings extends SettingsPreferenceFragment
             mStatusBarBatteryShowPercent.setSummary(
                     mStatusBarBatteryShowPercent.getEntries()[index]);
             return true;
-        } else if (preference == mStatusBarClock) {
-            int clockStyle = Integer.parseInt((String) newValue);
-            int index = mStatusBarClock.findIndexOfValue((String) newValue);
-            Settings.System.putInt(
-                    resolver, STATUS_BAR_CLOCK_STYLE, clockStyle);
-            mStatusBarClock.setSummary(mStatusBarClock.getEntries()[index]);
-            return true;
-        } else if (preference == mStatusBarAmPm) {
-            int statusBarAmPm = Integer.valueOf((String) newValue);
-            int index = mStatusBarAmPm.findIndexOfValue((String) newValue);
-            Settings.System.putInt(
-                    resolver, STATUS_BAR_AM_PM, statusBarAmPm);
-            mStatusBarAmPm.setSummary(mStatusBarAmPm.getEntries()[index]);
-            return true;
         }
         return false;
     }
@@ -166,13 +126,6 @@ public class CarbonStatusBarSettings extends SettingsPreferenceFragment
     @Override
     public void onResume() {
         super.onResume();
-        // Adjust clock position for RTL if necessary
-        Configuration config = getResources().getConfiguration();
-        if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-                mStatusBarClock.setEntries(getActivity().getResources().getStringArray(
-                        R.array.status_bar_clock_style_entries_rtl));
-                mStatusBarClock.setSummary(mStatusBarClock.getEntry());
-        }
         updateClockStyleDescription();
         updateBatteryBarDescription();
         updateTrafficMonitorDescription();
